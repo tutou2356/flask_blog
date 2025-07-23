a9f0640702ddf5d2a3c19cbde9d0c9358eb68e95
tutou2356 <142212931+tutou2356@users.noreply.github.com>
2025-07-24 01:48:04 +0800

    增加文章编辑功能

--- app_blog.py
+++ app_blog.py
@@ -7,7 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# flash() 函数需要一个密钥
 app.config['SECRET_KEY'] = 'dev'
 
 db = SQLAlchemy(app)
@@ -28,14 +27,11 @@ def init_db_command():
 
 # --- 路由逻辑 ---
 
-# 首页：展示所有文章
 @app.route('/')
 def index():
-    # 从数据库查询所有文章，按ID倒序排列（最新的在前面）
     posts = Post.query.order_by(Post.id.desc()).all()
     return render_template('index.html', posts=posts)
 
-# 创建新文章的页面
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -45,16 +41,36 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            # 创建新的Post对象并存入数据库
             new_post = Post(title=title, content=content)
             db.session.add(new_post)
             db.session.commit()
-            # 操作完成后，重定向到首页
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# 删除文章的功能
+# ❗️❗️❗️ 新增：编辑文章的路由
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    # 根据ID获取文章，如果找不到则返回404错误
+    post = Post.query.get_or_404(id)
+
+    if request.method == 'POST':
+        # 如果是提交表单，则更新数据
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            post.title = title
+            post.content = content
+            db.session.commit()
+            return redirect(url_for('index'))
+
+    # 如果是GET请求，则显示带有当前数据的编辑页面
+    return render_template('edit.html', post=post)
+
+
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
     post = Post.query.get_or_404(id)
@@ -62,7 +78,6 @@ def delete(id):
     db.session.commit()
     flash(f'"{post.title}" was successfully deleted!')
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
